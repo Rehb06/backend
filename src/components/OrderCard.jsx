ed2fd11857c67868dbf778077b2ef3138f2ac38c
Rehb06 <emilianohb.mj@gmail.com>
2023-12-09 21:36:54 -0600

    commit Pedidos5

--- src/components/OrderCard.jsx
+++ src/components/OrderCard.jsx
@@ -8,19 +8,19 @@ function OrderCard( {order}){
     return(
         <div className='bg-zinc-800 max-w-md w-full p-10 rounded-md'>
             <header className='flex justify-between'> 
-                <h1 className='text-1xl font-bold'>{product.name}</h1>
+                <h1 className='text-1xl font-bold'>{order.name}</h1>
                 <div className='flex gap-x-2 items-center'>
                     <button className='bg-red-500 hover:bg-red-600 
                     text-white px-4 py-2 rounded-lg'
                         onClick={()=>{
-                            //console.log(product._id)
-                            deleteOrder(product._id);
+                            
+                            deleteOrder(order._id);
                         }}
 
                     >
                         <IoTrashBinSharp/>
                     </button>
-                    <Link to={'/product/'+product._id}
+                    <Link to={'/pedidos/'+order._id}
                         className='bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg' 
                     >
 
