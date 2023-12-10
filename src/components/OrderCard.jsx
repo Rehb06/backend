efe7cd3c0f05cf53cee2993e9c038fed357fa2d5
Rehb06 <emilianohb.mj@gmail.com>
2023-12-09 21:30:43 -0600

    commit Pedidos4

--- src/components/OrderCard.jsx
+++ src/components/OrderCard.jsx
@@ -3,41 +3,47 @@ import { useOrders } from '../context/OrdersContext'
 import {Link} from 'react-router-dom'
 import { IoTrashBinSharp, IoPencilSharp} from 'react-icons/io5'
 
-function OrderCard({ order }) {
-    const { deleteOrder } = useOrders();
-
-    return (
+function OrderCard( {order}){
+    const {deleteOrder} = useOrders()
+    return(
         <div className='bg-zinc-800 max-w-md w-full p-10 rounded-md'>
-            <header className='flex justify-between'>
+            <header className='flex justify-between'> 
                 <h1 className='text-1xl font-bold'>{product.name}</h1>
                 <div className='flex gap-x-2 items-center'>
-                    <button
-                        className='bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg'
-                        onClick={() => {
-                            deleteOrder(order._id);
+                    <button className='bg-red-500 hover:bg-red-600 
+                    text-white px-4 py-2 rounded-lg'
+                        onClick={()=>{
+                            //console.log(product._id)
+                            deleteOrder(product._id);
                         }}
+
                     >
-                        <IoTrashBinSharp />
+                        <IoTrashBinSharp/>
                     </button>
-                    <Link
-                        to={`/order/${order._id}`}
-                        className='bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg'
+                    <Link to={'/product/'+product._id}
+                        className='bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg' 
                     >
-                        <IoPencilSharp />
+
+                        <IoPencilSharp/>
                     </Link>
+
                 </div>
             </header>
             <p className='text-slate-300 my-2'>
-                {order.Product}
+                {product.price}
+
             </p>
             <p className='text-slate-300 my-2'>
                 {order.DateOrder}
+
             </p>
             <p className='text-slate-300 my-2'>
                 {order.Status}
+
             </p>
+
         </div>
-    );
+    )
 }
 
-export default OrderCard;
+export default OrderCard
