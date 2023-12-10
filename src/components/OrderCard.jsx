57d9ec9ddb99ad2f80f68608bfae9258102e4f4a
Rehb06 <emilianohb.mj@gmail.com>
2023-12-09 21:09:42 -0600

    commit Pedidos3

--- src/components/OrderCard.jsx
+++ src/components/OrderCard.jsx
@@ -9,7 +9,7 @@ function OrderCard({ order }) {
     return (
         <div className='bg-zinc-800 max-w-md w-full p-10 rounded-md'>
             <header className='flex justify-between'>
-                <h1 className='text-1xl font-bold'>{order.name}</h1>
+                <h1 className='text-1xl font-bold'>{product.name}</h1>
                 <div className='flex gap-x-2 items-center'>
                     <button
                         className='bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg'
